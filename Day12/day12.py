e977eea6982945a343249c2b2ce06276590d1be8
Yabir <yabirgb@gmail.com>
2017-12-12 23:12:10 +0100

    Found a simplier version

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,63 +1,33 @@
-class Equivalence:
+pairs = {}
 
-    def __init__(self, integer):
-        self.elements = set()
-        self.elements.add(integer)
-
-    def __iadd__(self, other):
-        self.elements.update(other.objs())
-
-    def add(self, integer):
-        self.elements.add(integer)
-
-    def represents(self, integer):
-        return integer in self.elements
-
-    def objs(self):
-        return self.elements
-
-    def __contains__(self, key):
-        return self.represents(key)
-    
-    def __str__(self):
-        return repr(self.elements)
-
-    def __repr__(self):
-        return __str__()
-
-    def __len__(self):
-        return len(self.elements)
-
-    def __eq__(self, other):
-        return self.represents(key)
-
-eq = []
-
-with open("input.txt") as f:
-
-
+with open("input2.txt") as f:
     for line in f:
         line = line.replace("\n","").replace(",", "").split(" ")
-        key = int(line[0])
-        related = list(map(int, line[2:]))
-        target = None
-        
-        if key not in eq:
-            eq.append(Equivalence(key))
-            target = eq.index(key)
-        else:
-            target = eq.index(key)
+        pairs[int(line[0])] = set(map(int, line[2:]))
 
-        for rel in related:            
-            if rel in eq:
-                eq[target].add(rel)
-            else:
-                pos = eq.find(rel, target)
-                eq[target] += eq[pos]
-                del eq[pos]
+def search(num):
+    equiv = set([num])
+    start = set([num])
 
-for elem in eq:
-    print(elem)
+    while start:
 
-#zero = eq.index(0)
-#print(len(eq[zero]))
+        upgrade = set()
+        for elem in start:
+            upgrade.update(pairs[elem])
+
+        start = upgrade - equiv
+        equiv.update(upgrade)
+
+    return equiv
+            
+def search_all():
+    allkeys = set(pairs)
+    amount = 0
+    while allkeys:
+        amount += 1
+        group_last = search(allkeys.pop())
+        allkeys -=group_last
+
+    return amount
+print(len(search(0)))
+print(search_all())
