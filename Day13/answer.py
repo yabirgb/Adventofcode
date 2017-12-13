bc1023404e3a12240be7297d86ac8879c4975132
Yabir <yabirgb@gmail.com>
2017-12-13 17:08:58 +0100

    Finished day 13

--- Day13/answer.py
+++ Day13/answer.py
@@ -5,20 +5,40 @@ with open("input.txt") as f:
         line = line.replace(":", "").replace("\n","").split(" ")
         security.append((int(line[0]), int(line[1])))
 
-def penalty(delay):
+def penalty():
     penality = 0
 
     for level, size in security:
         positions = list(range(0, size - 1)) + list(range(size-1,0, -1))
-        guard_pos = positions[(level)%len(positions)]
+        guard_pos = positions[level%len(positions)]
 
         if guard_pos == 0:
             penality += level * size
 
     return penality
 
-print(penalty(0))
+print(penalty())
+
+#security = [(0,3), (1,2), (4,4), (6,4)]
+
 #Part 2
+def penaltyDelay(delay):
+    for level, size in security:
+        positions = list(range(0, size - 1)) + list(range(size-1,0, -1))
+        guard_pos = positions[(delay+level)%len(positions)]
 
+        if guard_pos == 0:
+            return False
 
+    return True
 
+ok = False
+seconds = 0
+
+while not ok:
+    if penaltyDelay(seconds) == True:
+        print(seconds)
+        ok = True
+    seconds += 1
+
+#print(total)
