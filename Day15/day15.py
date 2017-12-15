e8307157dcf465e38b9d3ba7fbf783f1ad5b9c34
Yabir <yabirgb@gmail.com>
2017-12-15 17:56:26 +0100

    Solutions to day 15

--- Day15/day15.py
+++ Day15/day15.py
@@ -7,12 +7,21 @@ inputB = 325
 def generator(n, r):
     while True:
         n = n * r % 2147483647
-        if n % r == 0:
+        yield n & 0xffff
+
+def generator2(n, r, k):
+    while True:
+        n = n * r % 2147483647
+        if not n % k:
             yield n & 0xffff
 
 genA = generator(inputA, 16807)
 genB = generator(inputB, 48271)
 
-counter = sum(next(genA) == next(genB) for _ in range(40000000))
+#counter = sum(next(genA) == next(genB) for _ in range(40000000))
 
+gen2A = generator2(inputA, 16807,4)
+gen2B = generator2(inputB, 48271,8)
+
+counter = sum(next(gen2A) == next(gen2B) for _ in range(5000000))
 print(counter)
